3038877ddb1de32f15f661c0629ac9edf565ffb6
ditrix <dmvoloshin@gmail.com>
2022-02-06 13:03:06 +0200

    product.crud  getall getone create

--- server/controllers/Product.js
+++ server/controllers/Product.js
@@ -1,17 +1,68 @@
+import {Product as ProductMapping} from '../models/mapping.js'
+import AppError from '../errors/AppError.js'
+
+
 class Product {
-    async getAll(req, res) {
-        res.status(200).send('Список всех товаров')
+    
+    async getAll(req, res, next) {
+
+        try {
+
+            const products = await ProductMapping.findAll();  
+            res.json(products)
+
+        } catch(e) {
+
+            next(AppError.badRequest(e.message))
+
+        }
+    
     }
 
-    async getOne(req, res) {
-        res.status(200).send('Получение одного товара')
+    async getOne(req, res, next) {
+        try {
+
+            if(!req.params.id){
+                 throw new Error('no request parameter id')
+            }
+
+            // const product = await ProductMapping.findOne({where: { id: req.params.id} });  
+            const product = await ProductMapping.findByPk(req.params.id)
+            if(!product){
+                 throw new Error('product not found')
+             }
+             res.json(product)
+
+        } catch(e) {
+
+            next(AppError.badRequest(e.message))
+
+        }
     }
 
-    async create(req, res) {
-        res.status(200).send('Создание нового товара')
+    async create(req, res, next) {
+
+        try {
+
+            const {name, price, image = '' } = req.body;
+            const product = await ProductMapping.create({name, price, image})
+            res.json(product)
+
+        } catch(e) {
+
+            next(AppError.badRequest(e.message))            
+
+        }
+
     }
 
     async update(req, res) {
+
+        // const product = await ProductMapping.findByPk(1)
+        // const name  = 'altimetr'
+        // const price = '10'
+        // await product.update({name,price})
+
         res.status(200).send('Обновление товара')
     }
 
